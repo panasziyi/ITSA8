d250faf09d7a82869f7a193b48bd7553e0abe63e
sjcha <sjcha@DESKTOP-H5G1ETO>
2023-03-18 16:21:42 +0800

    加入專案檔案。

---
+++ ITSA8/ITSA8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+    cin >> n;
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            break;
+        }
+    }
+    if (i == n) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
